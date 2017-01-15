84cabf255bd039c106e988a8765bb880ee24ebc0
Jason Chiang <jachian@Jasons-MacBook.local>
2017-01-14 17:30:21 -0800

    Cleaned utils.py. Added work on artist2vec.

--- utils.py
+++ utils.py
@@ -1,32 +1,57 @@
+from gensim.models import Word2Vec
 
+class Music2Vec(object):
+	def __init__(self, size='complete', split='train'):
+		self.tags_lut = {}
+		self.songs_lut = {}
+		self.uuid = None
+		self.corpus = None
+		self.appearances = None
+		self.MIN_SONGS = 10
 
-def get_data(size='small', split='train'):
-	fpath = 'data/yes_'+size+'/'+split+'.txt'
-	with open(fpath, 'r') as f:
-		mydata = f.read()
+		self.get_data(size, split)
+		self._create_hashes(size)
 
-	r_uuid, r_appearances, r_playlists = mydata.split('\n', 2)
-	c_uuid        = map(int, r_uuid.split())
-	c_appearances = map(int, r_appearances.split())
-	c_playlists   = [plist.split() for plist in r_playlists.split('\n')]
-	print 'Loading the {} data from the {} dataset'.format(split, size)
-	return c_uuid, c_appearances, c_playlists
+	def get_data(self, size, split):
+		fpath = 'data/yes_'+size+'/'+split+'.txt'
+		with open(fpath, 'r') as f:
+			mydata = f.read()
 
-def create_hashes(size='small'):
-	song_fpath = 'data/yes_'+size+'/song_hash.txt'
-	tag_fpath  = 'data/yes_'+size+'/tag_hash.txt'
+		r_uuid, r_appearances, r_corpus = mydata.split('\n', 2)
+		self.uuid = map(int, r_uuid.split())
+		self.appearances = map(int, r_appearances.split())
+		self.corpus = [plist.split() for plist in r_corpus.split('\n')]
+		print 'Loading the {} data from the {} dataset'.format(split, size)
 
-	with open(song_fpath, 'r') as f:
-		songs = [line.strip('\n').replace('w\\/', 'feat.').split('\t') for line in f.readlines()]
+	def _create_hashes(self, size):
+		tag_fpath = 'data/yes_'+size+'/tag_hash.txt'
+		song_fpath = 'data/yes_'+size+'/song_hash.txt'
 
-	songs_lut = {int(idx): {'artist': artist, 'song': song} for idx, song, artist in songs}
+		with open(song_fpath, 'r') as f:
+			songs = [line.strip('\n').replace('w\\/', 'feat.').split('\t') for line in f.readlines()]
 
-	with open(tag_fpath, 'r') as f:
-		tags = map(lambda x: x.split(', '), map(lambda x: x.strip('\n'), f.readlines()))
+		self.songs_lut = {int(idx): {'artist': artist, 'song': song} for idx, song, artist in songs}
 
-	tags_lut = {int(idx): tag for idx, tag in tags}
-	return songs_lut, tags_lut # remove later
+		with open(tag_fpath, 'r') as f:
+			tags  = map(lambda x: x.split(', '), map(lambda x: x.strip('\n'), f.readlines()))
 
-def clean_song2vec(playlist):
-	MIN_SONGS = 10
-	return filter(lambda x: len(x) > MIN_SONGS, playlist)
+		self.tags_lut = {idx: tag for idx, tag in tags}
+
+	def clean_song2vec(self):
+		return filter(lambda x: len(x) > self.MIN_SONGS, self.corpus)
+
+	def clean_artist2vec(self):
+		idx2artist = [map(lambda x: self.songs_lut[int(x)]['artist'], pl) for pl in self.corpus]
+		return filter(lambda x: len(x) > self.MIN_SONGS, idx2artist)
+
+def main():
+	mtv = Music2Vec()
+	corpus = mtv.clean_artist2vec()
+	# fpath = 'artist2vec_model.pkl'
+	# model = Word2Vec(corpus, seed=1738)
+	# model.save(fpath)
+	# print 'Model saved to {}'.format(fpath)
+	# model = Word2Vec.load(fpath)
+
+if __name__ == '__main__':
+	main()
